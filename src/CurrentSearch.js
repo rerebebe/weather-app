329db4b91cd82a19824acee5d5013073c27bf888
Regina <reginabb68@gmail.com>
2022-10-27 00:04:20 +0800

    .

--- src/CurrentSearch.js
+++ src/CurrentSearch.js
@@ -6,29 +6,42 @@ function CurrentSearch({ getTime }) {
     <div className="weatherlocation">
       <div className="location">
         {data.sys ? (
-          <p>
+          <h4>
             {data.name}, {data.sys.country}
+          </h4>
+        ) : null}
+      </div>
+      <div className="temprange">
+        {data.main ? (
+          <p>
+            {data.main.temp_min}°F ~ {data.main.temp_max}°F
           </p>
         ) : null}
       </div>
+
       <div className="temp">
         {data.main ? <h1>{data.main.temp}°F</h1> : null}
       </div>
       <div className="description">
         {data.weather ? <p>{data.weather[0].description}</p> : null}
       </div>
+
       <div className="bottom">
         <div className="humidity">
-          {data.main ? <p>Humidity: {data.main.humidity}%</p> : null}
-        </div>
-        <div className="wind">
           {data.main ? (
             <p>
-              Temperature: {data.main.temp_min}°F ~ {data.main.temp_max}°F
+              <span>Humidity</span> {data.main.humidity}%
             </p>
           ) : null}
         </div>
-        <div className="time">{data.dt ? <p>Time: {getTime()}</p> : null}</div>
+
+        <div className="time">
+          {data.dt ? (
+            <p>
+              <span>Time</span> {getTime()}
+            </p>
+          ) : null}
+        </div>
       </div>
     </div>
   );
